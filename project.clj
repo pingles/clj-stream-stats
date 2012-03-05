e3f49ccf33e029cd53cc39d0b17481816191832e
Paul Ingles <paul@oobaloo.co.uk>
2012-03-05 14:40:49 +0000

    change version to 0.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-stream-stats "1.0.0-SNAPSHOT"
+(defproject clj-stream-stats "0.0.1"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/math.numeric-tower "0.0.1"]])
